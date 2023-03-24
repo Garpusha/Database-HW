697be3cdcf04401e4bcac440c4b31018ba82fafe
Garpusha <theflash@yandex.ru>
2023-03-24 18:50:26 +0300

    Finish him

--- HW_5/main.py
+++ HW_5/main.py
@@ -5,12 +5,15 @@ from random import randint
 
 
 class UserRecords:
-
     def __init__(self, host_, port_, dbname_, user_, password_):
         self.connection = psycopg2.connect(host=host_, port=port_, dbname=dbname_, user=user_, password=password_)
 
-    def create_structure(self):
+    def create_structure(self):  # Create DB structure. Warning, all existing data will be removed!
         with self.connection.cursor() as my_cur:
+            my_cur.execute('DROP TABLE IF EXISTS phones;')
+            my_cur.execute('DROP TABLE IF EXISTS emails;')
+            my_cur.execute('DROP TABLE IF EXISTS users;')
+            self.connection.commit()
             my_cur.execute('''
             CREATE TABLE IF NOT EXISTS users ( 
             user_id SERIAL PRIMARY KEY,
@@ -33,14 +36,14 @@ class UserRecords:
             );''')
             self.connection.commit()
 
-    def load_data(self, table_name, file_name):
+    def load_data(self, table_name, file_name):  # Read data from CSV files into clean tables.
         with self.connection.cursor() as my_cur:
             my_cur.execute("""
             COPY %s FROM %s DELIMITER ';' CSV HEADER;
             """, (AsIs(table_name), file_name))
             self.connection.commit()
 
-    def input_record(self):
+    def input_record(self):  # User input. Returns entered user_id if correct, -1 otherwise.
         user_id = input('Enter UserID: ')
         if not user_id.isdecimal():
             print('Wrong ID, only decimal numbers allowed.')
@@ -54,7 +57,7 @@ class UserRecords:
                 return -1
         return user_id
 
-    def create(self, user_details):
+    def create(self, user_details):  # Create new record
         with self.connection.cursor() as my_cur:
             my_cur.execute("SELECT max(user_id) FROM users;")
             user_id = my_cur.fetchone()
@@ -82,33 +85,39 @@ class UserRecords:
                 """, (AsIs('phones'), phone_id, phone_no, user_id))
                 phone_id += 1
             self.connection.commit()
-            print(f'New record added:\nID: {user_id}, Name: {user_details[0]} {user_details[1]}')
-            print(f'E-mail: {user_details[2]} Phone(s): {user_details[3]}')
+            print(f'New record added:')
+            self.print_user_details(user_id)
 
-    def get_list(self):
+    def print_user_details(self, user_id):  # Print all user data using ID
         with self.connection.cursor() as my_cur:
             my_cur.execute("""
-            SELECT user_id, name, surname FROM users;
+            SELECT name, surname FROM users WHERE user_id = %s;
+            """, (user_id,))
+            record_ = my_cur.fetchone()
+            print(f'{user_id}. {record_[0]} {record_[1]}')
+            my_cur.execute("""
+            SELECT email FROM emails
+            WHERE user_id = %s;""", (user_id,))
+            emails_ = my_cur.fetchall()
+            print(f'E-mails: ', end=' ')
+            [print(f'{email_}', end=' ') for email_ in emails_]
+            print(f'\nPhones: ', end=' ')
+            my_cur.execute("""
+            SELECT phone_no FROM phones
+            WHERE user_id = %s;""", (user_id,))
+            phones_ = my_cur.fetchall()
+            [print(phone_[0], end=' ') for phone_ in phones_]
+            print('\n')
+
+    def get_list(self):  # Get all users data
+        with self.connection.cursor() as my_cur:
+            my_cur.execute("""
+            SELECT user_id FROM users;
             """)
             records_ = my_cur.fetchall()
-            for record_ in records_:
-                print(f'{record_[0]}. {record_[1]} {record_[2]}')
-                # user_id = str(record_[0])
-                my_cur.execute("""
-                SELECT email FROM emails
-                WHERE user_id = %s;""", (record_[0],))
-                emails_ = my_cur.fetchall()
-                print('Emails:', end=' ')
-                [print(email_[0], end=' ') for email_ in emails_]
-                my_cur.execute("""
-                SELECT phone_no FROM phones
-                WHERE user_id = %s;""", (record_[0],))
-                phones_ = my_cur.fetchall()
-                print('\nPhones:', end=' ')
-                [print(phone_[0], end=' ') for phone_ in phones_]
-                print('\n')
+            [self.print_user_details(record_[0]) for record_ in records_]
 
-    def add_phone(self):
+    def add_phone(self):  # Add new phone number to the existing user
         user_id = self.input_record()
         if user_id == -1:
             return
@@ -123,7 +132,7 @@ class UserRecords:
             self.connection.commit()
             print(f'Phone number {phone_} was added to UserID {user_id}')
 
-    def change(self):
+    def change(self):  # Update existing record. New data will be generated automatically.
         user_id = self.input_record()
         if user_id == -1:
             return
@@ -141,21 +150,20 @@ class UserRecords:
             self.connection.commit()
             my_cur.execute("SELECT count(phone_no) FROM phones WHERE user_id = %s;", (user_id,))
             phones_ = my_cur.fetchone()[0]
-            if phones_ !=0:
+            if phones_ != 0:
                 my_cur.execute("SELECT phone_id FROM phones WHERE user_id = %s;", (user_id,))
                 phone_ids_ = my_cur.fetchall()
                 phone_ids_ = [(phone_id_[0]) for phone_id_ in phone_ids_]
                 user_details[3] = []
                 for index, phone_id_ in enumerate(phone_ids_):
                     user_details[3].append(randint(1000000, 9999999))
-                    my_cur.execute("UPDATE phones SET phone_no = %s WHERE phone_id = %s;", (user_details[3][index], phone_id_))
+                    my_cur.execute("UPDATE phones SET phone_no = %s WHERE phone_id = %s;",
+                                   (user_details[3][index], phone_id_))
                 self.connection.commit()
-            print(f'Record updated: UserID {user_id}')
-            print(f'{user_details[0]} {user_details[1]}, {user_details[2]}')
-            [print(index, end=' ') for index in user_details[3]]
-            print('\n')
+            print('Record updated.')
+            self.print_user_details(user_id)
 
-    def delete_phone(self):
+    def delete_phone(self):  # Delete existing phone number
         user_id = self.input_record()
         if user_id == -1:
             return
@@ -189,7 +197,7 @@ class UserRecords:
                 self.connection.commit()
                 print(f'Phone {phones_[choice_]} from UserID {user_id} deleted.')
 
-    def delete(self):
+    def delete(self):  # Delete existing record
         with self.connection.cursor() as my_cur:
             my_cur.execute('SELECT user_id, name, surname FROM users;')
             records_ = my_cur.fetchall()
@@ -203,17 +211,59 @@ class UserRecords:
             self.connection.commit()
             print(f'Record UserID {user_id} was deleted.')
 
+    def find(self):  # Find existing user by parameters
+        with self.connection.cursor() as my_cur:
+            print('Search by:\n 1. Name\n2. Surname\n3. E-mail\n4. Phone\n')
+            choice_ = input('Enter option: ')
+            if choice_ == '1':
+                choice_ = input('Enter name: ')
+                my_cur.execute('''SELECT COUNT(name), user_id FROM users WHERE name = %s
+                               GROUP BY user_id;''', (choice_,))
+                user_details_ = my_cur.fetchone()
+                if user_details_ == None:
+                    print(f'User {choice_} not found.')
+                    return
+                self.print_user_details(user_details_[1])
+            elif choice_ == '2':
+                choice_ = input('Enter surname: ')
+                my_cur.execute('''SELECT COUNT(name), user_id FROM users WHERE surname = %s
+                GROUP BY user_id;''', (choice_,))
+                user_details_ = my_cur.fetchone()
+                if user_details_ == None:
+                    print(f'User {choice_} not found.')
+                    return
+                self.print_user_details(user_details_[1])
+            elif choice_ == '3':
+                choice_ = input('Enter e-mail: ')
+                my_cur.execute('''SELECT COUNT(email), user_id FROM emails WHERE email = %s
+                               GROUP BY user_id;''', (choice_,))
+                user_details_ = my_cur.fetchone()
+                if user_details_ == None:
+                    print(f'User with e-mail {choice_} not found.')
+                    return
+                self.print_user_details(user_details_[1])
+            elif choice_ == '4':
+                choice_ = input('Enter phone: ')
+                my_cur.execute('''SELECT COUNT(phone_no), user_id FROM phones WHERE phone_no = %s
+                               GROUP BY user_id;''', (choice_,))
+                user_details_ = my_cur.fetchone()
+                if user_details_ == None:
+                    print(f'User with phone number {choice_} not found.')
+                    return
+                self.print_user_details(user_details_[1])
+            else:
+                print('Wrong input.')
+            return
 
 
-        
-def read_config(path, section, parameter):
+def read_config(path, section, parameter):  # Reading configs
     config = configparser.ConfigParser()
     config.read(path)
     c_value = config.get(section, parameter)
     return c_value
 
 
-def get_new_identity():
+def get_new_identity():  # Generate user data
     phone_ = []
     names = ['John', 'Peter', 'Tanya', 'Elena', 'Bill', 'Phil', 'Andrew', 'Alex', 'Joanna', 'Ivanna', 'Iren',
              'Sam', 'Bruce', 'Kylie', 'Daniel', 'Linda', 'Mary', 'Chris', 'Christina', 'Penny']
@@ -239,9 +289,9 @@ path_to_csv = read_config('config.ini', 'Main', 'DataPath')
 
 my_record = UserRecords(host, port, db_name, user_name, password)
 my_record.create_structure()
-# my_record.load_data('users', f'{path_to_csv}users.csv')
-# my_record.load_data('emails', f'{path_to_csv}emails.csv')
-# my_record.load_data('phones', f'{path_to_csv}phones.csv')
+my_record.load_data('users', f'{path_to_csv}users.csv')
+my_record.load_data('emails', f'{path_to_csv}emails.csv')
+my_record.load_data('phones', f'{path_to_csv}phones.csv')
 
 while True:
     print('-' * 20)
@@ -262,6 +312,7 @@ while True:
         my_record.delete_phone()
     elif choice == '5':
         my_record.delete()
-
+    elif choice == '6':
+        my_record.find()
     elif choice == '7':
         my_record.get_list()
